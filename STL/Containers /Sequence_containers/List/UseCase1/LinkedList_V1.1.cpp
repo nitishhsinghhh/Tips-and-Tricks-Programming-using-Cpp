ba14dc49b75f5c02ae48f51ae7d1e3d7f0c4352d
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2024-07-07 20:08:38 +0530

    Update LinkedList_V1.1.cpp

--- STL/Containers /Sequence_containers/List/UseCase1/LinkedList_V1.1.cpp
+++ STL/Containers /Sequence_containers/List/UseCase1/LinkedList_V1.1.cpp
@@ -1,47 +1,55 @@
 #include <iostream>
 
+// Definition of ListNode structure
 struct ListNode {
 	int val;
 	ListNode *next;
 	ListNode *prev;
 
+	// Default constructor
 	ListNode() : val(0), next(nullptr), prev(nullptr) {}
+
+	// Constructor with a value
 	ListNode(int x) : val(x), next(nullptr), prev(nullptr) {}
+
+	// Constructor with value, next pointer, and prev pointer
 	ListNode(int x, ListNode *next, ListNode *prev) : val(x), next(next), prev(prev) {}
+
+	// Destructor
+    	~ListNode() {
+		// Delete the next node in the list
+		delete next; // This will recursively delete the entire list
+	}
 };
 
 int main() {
 	ListNode *head = nullptr;
 	ListNode *prev = nullptr;
 
-	// Create and link the nodes in a loop
+	// Creating and link the nodes in a loop
 	for (int i = 1; i <= 5; i++) {
 		ListNode *node = new ListNode(i, nullptr, prev);
-		if (prev != nullptr) {
+		if (prev != nullptr) 
 			prev->next = node;
-		}
-		else {
+		else 
 			head = node; // Set head to the first node
-		}
 		prev = node;
 	}
 
-	// Traverse the linked list forwards
+	// Traversing the list forward
 	ListNode *current = head;
 	while (current != nullptr) {
 		std::cout << current->val << " ";
 		current = current->next;
 	}
-
 	std::cout << std::endl;
 
-	// Traverse the linked list backwards
+	// Traversing the list backward
 	current = prev;
 	while (current != nullptr) {
 		std::cout << current->val << " ";
 		current = current->prev;
 	}
-
 	std::cout << std::endl;
 
 	// Clean up memory by deleting nodes
